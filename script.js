438e2af68a82c9015b68745987cae3a123d22830
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-18 10:48:16 +0100

    Create operator function

--- script.js
+++ script.js
@@ -2,35 +2,59 @@
 
 function add (a, b) {
     let c = a + b;
-    console.log(c);
+    return c;
 }
 
-add(5, 5); // 10
+console.log(add(5, 5)); // 10
 
 // Subtraction
 
 function subtract (a, b) {
     let c = a - b;
-    console.log(c);
+    return c;
 }
 
-subtract(5, 5); // 0
+console.log(subtract(5, 5)); // 0
 
 // Multiplication
 
 function multiply (a, b) {
     let c = a * b;
-    console.log(c);
+    return c;
 }
 
-multiply(5, 5); // 25
+console.log(multiply(5, 5)); // 25
 
 // Division
 
 function divide (a, b) {
     let c = a / b;
-    console.log(c);
+    return c;
 }
 
-divide(10, 2); // 5
+console.log(divide(10, 2)); // 5
+
+// Operator function to tke the operator and two numbers, and call one of the above functions on the numbers
+
+let firstNum = 10;
+let operator = '+';
+let secondNum = 2;
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+          return add(num1, num2);
+        case '-':
+          return subtract(num1, num2);
+        case '*':
+          return multiply(num1, num2);
+        case '/':
+          return divide(num1, num2);
+      }
+}
+
+let result = operate(firstNum, operator, secondNum);
+
+console.log(result);
+
 
