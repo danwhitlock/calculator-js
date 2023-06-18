433ed2640439091fdb3f67634cb163d3c12678ef
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-18 10:17:57 +0100

    Create basic add, subtract, multiply & divide functions

---
+++ script.js
@@ -0,0 +1,36 @@
+// Addition
+
+function add (a, b) {
+    let c = a + b;
+    console.log(c);
+}
+
+add(5, 5); // 10
+
+// Subtraction
+
+function subtract (a, b) {
+    let c = a - b;
+    console.log(c);
+}
+
+subtract(5, 5); // 0
+
+// Multiplication
+
+function multiply (a, b) {
+    let c = a * b;
+    console.log(c);
+}
+
+multiply(5, 5); // 25
+
+// Division
+
+function divide (a, b) {
+    let c = a / b;
+    console.log(c);
+}
+
+divide(10, 2); // 5
+
